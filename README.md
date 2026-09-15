9fa834ec195bd47fa40e4c78d7d3ed8d563323ad
sunsun0209s <sunsun0209s@gmail.com>
2026-09-15 22:54:46 +0800

    chore: apply gitignore and clean cache

---
+++ README.md
@@ -0,0 +1,3 @@
+# 03 - Ngôn ngữ lập trình C# (Mã MH: 841423 - Nhóm: 04)
+- Số tín chỉ: 4
+- Lập trình C# / .NET 8.0, OOP, Collections, LINQ, Task Parallel / Async-Await, Entity Framework, UI (.NET MAUI / WPF / ASP.NET).
---
+++ Tuan01/ThucHanh/NNLTCsharpC4/Buoi01Prj/Program.cs
@@ -0,0 +1,19 @@
+﻿using MyLib;
+namespace Buoi01Prj;
+
+
+
+public class Program1
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Main 1");
+    }
+}
+public class Program2
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Main 2");
+    }
+}
