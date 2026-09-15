d544abf253e087727640ad6ef638ab5c96b33f14
sunsun0209s <sunsun0209s@gmail.com>
2026-09-16 00:37:38 +0800

    Bai 1

---
+++ Tuan01/Lab01/Lab01Prj/Bai1.cs
@@ -0,0 +1,11 @@
+namespace Lab01Prj;
+
+public class Bai1
+{
+    public static void Run()
+    {
+        Console.Write("Nhap ho ten cua ban: ");
+        string? hoTen = Console.ReadLine();
+        Console.WriteLine($"Chao ban {hoTen}!");
+    }
+}
---
+++ Tuan01/Lab01/Lab01Prj/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Lab01Prj;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Bai1.Run();
+    }
+}
